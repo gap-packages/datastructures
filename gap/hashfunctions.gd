ab611a090a6051b9d5a84331600958e00ede6c1f
Markus Pfeiffer <markus.pfeiffer@st-andrews.ac.uk>
2018-07-02 11:27:35 +0100

    Hash documentation

--- gap/hashfunctions.gd
+++ gap/hashfunctions.gd
@@ -12,24 +12,24 @@
 #!
 #! @Section Introduction
 #!
-#! A hash function in datapackages is a function H which
-#! maps a value X to a small integer (where a small integer
-#! is an integer in the range [-2^28..2^28-1] on a 32-bit
-#! system, and [-2^60..2^60-1] on a 64-bit system), under
-#! the requirement that if X=Y, then H(X)=H(Y).
+#! A hash function in <Package>datastructures</Package> is a
+#! function H which maps a value X to a small integer (where
+#! a small integer is an integer in the range [-2^28..2^28-1]
+#! on a 32-bit system, and [-2^60..2^60-1] on a 64-bit system),
+#! under the requirement that if X=Y, then H(X)=H(Y).
 #!
-#! A variety of hash functions are provided by datastructures, with
+#! A variety of hash functions are provided by
+#! <Package>datastructures</Package>, with
 #! different behaviours. A bad choice of hash function
 #! can lead to serious performance problems.
 #!
 
-
 #! @Section Hash Functions
 
-#! Hashes any values built from the basic built-in types
-#! of GAP, namely
-#! integers, booleans, lists, records, permutations,
-#! transformations and partial permutations.
+#! @Description
+#! Hashes any values built inductively from the basic built-in types
+#! of &GAP;, namely integers, booleans, lists, records, permutations,
+#! transformations and partial permutations, and lists and records.
 #!
 #! Also this function will takes a variadic length
 #! list of arguments, treating more than one argument
@@ -37,11 +37,14 @@
 #! HashBasic(x,y,z) = HashBasic([x,y,z]).
 DeclareGlobalFunction("HashBasic");
 
+#! @Description
 #! datastructures provides two hash functions for permutation groups.
 #! Hash_PermGroup_Fast is faster, but will return the same
-#! value for groups with the same size, orbits and transitivity.
+#! value for groups with the same size, orbits and degree of
+#! transitivity.
 DeclareGlobalFunction("Hash_PermGroup_Fast");
 
+#! @Description
 #! datastructures provides two hash functions for permutation groups.
 #! Hash_PermGroup_Complete is slower, but is extremely unlikely
 #! to return the same hash for two different groups.

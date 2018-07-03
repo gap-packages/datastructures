c0fc790f1a84fd13912568f7311a355adb29694c
Markus Pfeiffer <markus.pfeiffer@st-andrews.ac.uk>
2018-07-03 14:05:52 +0100

    Update documentation for heaps

--- gap/binaryheap.gd
+++ gap/binaryheap.gd
@@ -10,9 +10,30 @@
 
 #! @Chapter Heaps
 #!
-#! @Section Binary heaps
+#! @Section Binary Heaps
+#! @SectionLabel BinaryHeap
 #!
+#! A binary heap employs a binary tree as its underlying tree datastructure.
+#! The implemenataion of binary heaps in <Package>datastructures</Package> stores
+#! this tree in a flat array which makes it a very good and fast default choice for
+#! general purpose use. In particular, even though other heap implementations have
+#! better theoretical runtime bounds, well-tuned binary heaps outperform them
+#! in many applications.
+#!
+#! For some reference see <URL>http://stackoverflow.com/questions/6531543</URL>
+
+#! @Description
+#! Constructor for binary heaps. The optinal argument <A>isLess</A> must be a binary function
+#! that performs comparison between two elements on the heap, and returns <K>true</K> if the first
+#! argument is less than the second, and <K>false</K> otherwise.
+#! Using the optional argument <A>data</A> the user can give a collection of initial values that
+#! are pushed on the stack after construction.
+#! @Arguments [isLess, [data]]
+#! @Returns A binary heap
+DeclareGlobalFunction("BinaryHeap");
+
+#! @Section Declarations
 DeclareRepresentation( "IsBinaryHeapFlatRep", IsHeap and IsPositionalObjectRep, []);
 BindGlobal( "BinaryHeapType", NewType(HeapFamily, IsBinaryHeapFlatRep and IsMutable));
 
-DeclareGlobalFunction("BinaryHeap");
+
--- gap/heap.gd
+++ gap/heap.gd
@@ -12,19 +12,14 @@
 ##  This file defines the interface for heaps.
 ##
 
-## Heaps are always max-heaps, but there should be a way
-## to pass a comparison function via an options record, which
-## in effect enables the making of min heaps
-
 #! @Chapter Heaps
 #!
 #! @Section Introduction
-#! A <E>heap</E > is a tree datastructure that stores items that can be compared
-#! using an ordering relation $\leq$ such that for any child $C$ of a node $N$
-#! it holds that $C \leq N$.
+#! A <E>heap</E > is a tree datastructure  such that for any child $C$ of a node $N$
+#! it holds that $C \leq N$, according to some ordering relation $\leq$.
 #!
 #! The fundamental operations for heaps are Construction, <C>Push</C>ing data
-#! onto the heap <C>Peek</C>ing at the topmost item, and <C>Pop</C>ping the
+#! onto the heap, <C>Peek</C>ing at the topmost item, and <C>Pop</C>ping the
 #! topmost item off of the heap.
 #!
 #! For a good heap implementation these basic operations should not exceed
@@ -34,34 +29,74 @@
 # TODO: Give theoretical bounds for our implementations in the documentation,
 # and provide some test evidence.
 #!
-#! We currently provide two types of heaps: <Ref Label="BinaryHeap"/> and
-#! <Ref Label="PairingHeap" />.
+#! We currently provide two types of heaps: Binary Heaps <Ref Sect='Section_BinaryHeap'/> and
+#! Pairing Heaps <Ref Sect="Section_PairingHeap" />.<P/>
+#!
+#! The following code shows how to use a binary heap.
+#! @BeginExample
+#! gap> h := BinaryHeap();
+#! <binary heap with 0 entries>
+#! gap> Push(h, 5);
+#! gap> Push(h, -10);
+#! gap> Peek(h);
+#! 5
+#! gap> Pop(h);
+#! 5
+#! gap> Peek(h);
+#! -10
+#! @EndExample
+#!
+#! The following code shows how to use a pairing heap.
+#! @BeginExample
+#! gap> h := PairingHeap( {x,y} -> x.rank > y.rank );
+#! <pairing heap with 0 entries>
+#! gap> Push(h, rec( rank  := 5 ));
+#! gap> Push(h, rec( rank  := 7 ));
+#! gap> Push(h, rec( rank  := -15 ));
+#! gap> h;
+#! <pairing heap with 3 entries>
+#! gap> Peek(h);
+#! rec( rank := -15 )
+#! gap> Pop(h);
+#! rec( rank := -15 )
+#! @EndExample
 #!
 #! @EndSection
 
 #! @Section API
 #!
-#! Every implementation of a heap must follow the API described in this
-#! section.
+#! For the purposes of the <Package>datastructures</Package>, we provide
+#! a category <Ref Func="IsHeap"/> . Every implementation of a heap in the
+#! category <Ref Func="IsHeap"/> must follos the API described in this section.
 #!
 
 #! @Description
-#! Category of heaps
+#! The category of heaps. Every object in this category promises to
+#! support the API described in this section.
 DeclareCategory("IsHeap", IsObject);
 BindGlobal( "HeapFamily", NewFamily("HeapFamily") );
 
-#TODO Do we want to use constructors?
-# Arguments are a heap filter, a comparison function, and maybe some data?
+
+#! @Description
+#! Wrapper function around constructors
+DeclareGlobalFunction("Heap");
+
+#! @Description
+#! Construct a new heap
+#!
+#! @Arguments [filter, func, data]
+#! @Returns a heap
 DeclareConstructor("NewHeap", [IsHeap, IsObject, IsObject]);
 
 #! @Description
-#! Throws the object <A>object</A> a new object onto <A>heap</A>.
+#! Puts the object <A>object</A> a new object onto <A>heap</A>.
 #!
 #! @Arguments heap, object
 DeclareOperation("Push", [IsHeap, IsObject]);
 
 #! @Description
 #! Inspect the item at the top of <A>heap</A>.
+#!
 #! @Arguments heap
 DeclareOperation("Peek", [IsHeap]);
 
@@ -76,25 +111,5 @@ DeclareOperation("Pop", [IsHeap]);
 #! @Arguments heap1, heap2
 DeclareOperation("Merge", [IsHeap, IsHeap]);
 
-#! @EndSection
-
-#!
-#! @Section Examples
-#!
-#! @BeginExample
-#! gap> h := BinaryHeap();
-#! gap> Push(h, 5);
-#! gap> Push(h, -10);
-#! gap> Peek(h);
-#! gap> Pop(h);
-#! @EndExample
-#!
-#! @BeginExample
-#! gap> h := PairingHeap( {x,y} -> x.rank > y.rank );
-#! gap> Push(h, rec( rank  := 5 ));
-#! gap> Push(h, rec( rank  := 7 ));
-#! gap> Push(h, rec( rank  := -15 ));
-#! gap> Pop(h);
-#! @EndExample
-#!
-#! @EndSection
+# TODO: find out how to properly docuymnt this without a `DeclareOperation`
+#! Heaps also support <Ref Func="IsEmpty"/> and <Ref Func="Size"/>
